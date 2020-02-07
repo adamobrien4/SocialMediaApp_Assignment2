1a423e611ed6d5a6c4efcdad8d34c8e1f82c49f3
Adam O'Brien <17226864@studentmail.ul.ie>
2020-02-07 10:04:45 +0000

    Added FirebaseFirestore query for recent posts

--- app/src/main/java/ie/ul/socialmediaappassignment2/MainActivity.java
+++ app/src/main/java/ie/ul/socialmediaappassignment2/MainActivity.java
@@ -11,9 +11,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,20 +26,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Andrew has gay
     }
 
     public void getData(View view) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
 
-        db.collection("drinks")
-                .get()
+        CollectionReference colRef = db.collection("test-collection");
+
+        Query query = colRef.orderBy("timestamp", Query.Direction.DESCENDING).limit(2);
+
+        query.get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        System.out.println("OnCompleteListerner");
                         if (task.isSuccessful()) {
+                            System.out.println("Task ResultSet size : " + task.getResult().size());
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                System.out.println(document.getId());
-                                System.out.println(document.getData());
+                                System.out.println(document.getId() + " => " + document.getData());
                             }
                         } else {
                             Log.w("tag", "Error getting documents", task.getException());
@@ -48,12 +61,12 @@ public class MainActivity extends AppCompatActivity {
     public void storeData(View view) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        CollectionReference drinks = db.collection("drinks");
+        CollectionReference drinks = db.collection("test-collection");
 
         Map<String, Object> drink1 = new HashMap<>();
-        drink1.put("name", "Espresso");
-        drink1.put("description", "It's a strong coffee!");
-        drink1.put("price", 3.50);
-        drinks.document("espresso").set(drink1);
+        drink1.put("name", "Adam");
+        drink1.put("message", "This is my message");
+        drink1.put("timestamp", System.currentTimeMillis());
+        drinks.document("message2").set(drink1);
     }
 }
