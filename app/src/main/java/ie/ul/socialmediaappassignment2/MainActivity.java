f1810d21d418ab39f1bce1c5ae1b83fdbd7039d3
Adam O'Brien <17226864@studentmail.ul.ie>
2020-02-12 18:54:58 +0000

    Removed debugging elements from MainActivity Activity User is directed to ViewPosts Activity on successful login

--- app/src/main/java/ie/ul/socialmediaappassignment2/MainActivity.java
+++ app/src/main/java/ie/ul/socialmediaappassignment2/MainActivity.java
@@ -1,33 +1,19 @@
 package ie.ul.socialmediaappassignment2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.internal.api.FirebaseNoSignedInUserException;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 private static final int RC_SIGN_IN = 123;
@@ -60,6 +46,7 @@ private static final int RC_SIGN_IN = 123;
                         "name = " + user.getDisplayName()+"\n"+
                         "email = " + user.getEmail() + "\n" +
                         "id = "+ user.getUid());
+                openPostsActivity();
             } else{
                 if(response == null){
                     System.out.println("Sign in cancelled"); return;
@@ -71,47 +58,7 @@ private static final int RC_SIGN_IN = 123;
         }
     }
 
-    public void getData(View view) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setTimestampsInSnapshotsEnabled(true)
-                .build();
-        db.setFirestoreSettings(settings);
-
-        CollectionReference colRef = db.collection("test-collection");
-
-        Query query = colRef.orderBy("timestamp", Query.Direction.DESCENDING).limit(2);
-
-        query.get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        System.out.println("OnCompleteListerner");
-                        if (task.isSuccessful()) {
-                            System.out.println("Task ResultSet size : " + task.getResult().size());
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                System.out.println(document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w("tag", "Error getting documents", task.getException());
-                        }
-                    }
-                });
-    }
-
-    public void storeData(View view) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        CollectionReference drinks = db.collection("test-collection");
-
-        Map<String, Object> drink1 = new HashMap<>();
-        drink1.put("name", "Adam");
-        drink1.put("message", "This is my message");
-        drink1.put("timestamp", System.currentTimeMillis());
-        drinks.document("message2").set(drink1);
-    }
-
-    public void openPostsActivity(View view) {
+    public void openPostsActivity() {
         Intent intent = new Intent(this, PostFeedActivity.class);
         startActivity(intent);
     }
